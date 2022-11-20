d170d84bd05caf2be0b6542f4504d70ae7ac05a1
Lyra <1170158548@qq.com>
2022-11-20 23:01:16 +0800

    just fine

--- components/Post/Id/Toc.tsx
+++ components/Post/Id/Toc.tsx
@@ -2,22 +2,14 @@ import { useLayoutEffect } from "react";
 import TocHelper from "toc-helper";
 
 const Toc = ()=>{
-    useLayoutEffect(() => {
-        console.log("Toc")
-        if(document.getElementById('post-toc')&&document.getElementById('post-content')){
-            console.log(          
-                new TocHelper(document.getElementById('post-toc'), {
-                scrollSelector: document.getElementById('post-content'),
-                contentSelector: document.getElementById('post-content'),
-                collapsedLevel: 3,
-              }))
-        }
-        //   new TocHelper(document.getElementById('post-toc'), {
-        //     scrollSelector: document.getElementById('post-content'),
-        //     contentSelector: document.getElementById('post-content'),
-        //     collapsedLevel: 3,
-        //   });
-      }, []);
+    // useLayoutEffect(() => {
+    //     console.log("Toc")
+    //       new TocHelper(document.getElementById('post-toc'), {
+    //         scrollSelector: document.getElementById('post-content'),
+    //         contentSelector: document.getElementById('post-content'),
+    //         collapsedLevel: 3,
+    //       });
+    //   }, []);
   return (
     <div
     id="post-toc"
