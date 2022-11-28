4d9559cf18a61d905ceaf13a7e1e891c5473a2fa
Lyra <1170158548@qq.com>
2022-11-28 17:34:16 +0800

    fix

--- components/Tag/Main/loading.tsx
+++ components/Tag/Main/loading.tsx
@@ -3,7 +3,7 @@ import { SkeletonCard,LeftBarSkeletonCard } from './skeletonCard'
 const loading = () => {
   return (
     <>
-      <section className="h-full border-r  w-1/5 space-y-2">
+    <section className="h-[10%] md:h-full md:border-r  md:w-1/5 space-y-2">
     <div className="w-full flex-row flex items-center pr-2">
       <hgroup className="flex items-center flex-row relative pr-2 bg-white space-x-1 text-lg">
               <p className=" text-red-500 bg-red-500 rounded-md">集册</p>
@@ -12,16 +12,22 @@ const loading = () => {
       </hgroup>
       <p className="flex-grow bg-neutral-100  h-[1px]"/>
       </div>
-      <section className="h-full">
-        <ul className="space-y-2  overflow-y-auto">
+        <ul className="
+                flex flex-row w-full 
+                space-x-2 
+                md:flex-col 
+                md:space-x-0
+                md:h-full 
+                md:space-y-2 
+                overflow-auto
+        ">
         <LeftBarSkeletonCard/>
        <LeftBarSkeletonCard/>
         <LeftBarSkeletonCard/>
 
         </ul>
-      </section>
     </section>
-    <section className="px-4 w-4/5 space-y-2 h-full">
+    <section className="h-[80%] md:px-4 md:w-4/5 space-y-2 md:h-[95%]">
     <div className="w-full flex-row flex items-center">
     <hgroup className="flex items-center relative z-20 pr-2 bg-white space-x-1 text-2xl">
             <p className=" text-red-500 bg-red-500 rounded-md">xxxx</p>
@@ -30,14 +36,12 @@ const loading = () => {
     </hgroup>
     <p className="flex-grow bg-neutral-100  h-[2px]"/>
     </div>
-    <section className="h-full">
-      <ul className="space-y-4 overflow-y-auto scrollbar-none h-full">
+      <ul className="h-full space-y-4 overflow-y-auto scrollbar-none">
       <SkeletonCard/>
       <SkeletonCard/>
       <SkeletonCard/>
       <SkeletonCard/>
       </ul>
-    </section>
   </section>
 
     </>
