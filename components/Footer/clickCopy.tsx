0836c99a1e9c36718fe3b2762b9ad575eeb09f9f
Lyra <1170158548@qq.com>
2022-12-04 19:47:09 +0800

    fixing

--- components/Footer/clickCopy.tsx
+++ components/Footer/clickCopy.tsx
@@ -2,13 +2,12 @@ import Link from 'next/link'
 import { useRef } from 'react';
 
 const clickCopy = ({link,children}:{link:string,children:any}) => {
-    const textRef = useRef(null)
     const textareaRef = useRef(null)
     const copyText = ()=> {
-        var text = textRef.current.innerHTML;
         var input = textareaRef.current;
-        input.value = text;
+        input.value = link;
         input.select();//选中文本
+        console.log(input)
         document.execCommand("copy");
         alert("复制了Email :)")
     }
@@ -19,8 +18,7 @@ const clickCopy = ({link,children}:{link:string,children:any}) => {
     text-xs
     font-normal text-neutral-500
     hover:text-red-500 hover:border-b-red-500 transition duration-300'>
-    <p className='hidden' ref={textRef}>{link}</p>
-    <textarea className='hidden' ref={textareaRef}></textarea>
+    <textarea className=' absolute opacity-0 w-0 h-0' ref={textareaRef}></textarea>
     {children}
     </li>
     </button>
--- components/Mobile/Dashboard/footerClickCopy.tsx
+++ components/Mobile/Dashboard/footerClickCopy.tsx
@@ -1,12 +1,10 @@
 import { useRef } from 'react';
 
 const FooterClickCopy = ({link,children}:{link:string,children:any}) => {
-    const textRef = useRef(null)
     const textareaRef = useRef(null)
     const copyText = ()=> {
-        var text = textRef.current.innerHTML;
         var input = textareaRef.current;
-        input.value = text;
+        input.value = link;
         input.select();//选中文本
         document.execCommand("copy");
         alert("复制了Email :)")
@@ -22,8 +20,7 @@ const FooterClickCopy = ({link,children}:{link:string,children:any}) => {
     hover:bg-white 
     hover:border-b-red-500
     transition duration-300'>
-    <p className='hidden' ref={textRef}>{link}</p>
-    <textarea className='hidden' ref={textareaRef}></textarea>
+    <textarea className=' absolute opacity-0 w-0 h-0'  ref={textareaRef}></textarea>
     {children}
     </li>
     </button>
